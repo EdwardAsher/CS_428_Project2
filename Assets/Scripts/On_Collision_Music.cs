f296cfefdc091a05a87cc3808357dcad1de89d0a
Edward <ereyes28@uic.edu>
2019-10-12 19:56:32 -0500

    Added model Reactions

--- Assets/Scripts/On_Collision_Music.cs
+++ Assets/Scripts/On_Collision_Music.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class On_Collision : MonoBehaviour
+public class On_Collision_Music : MonoBehaviour
 {
+    AudioSource music;
+    bool setActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        music = GetComponent<AudioSource>();
+        music.Stop();
+        setActive = false;
     }
 
     // Update is called once per frame
@@ -22,7 +26,17 @@ public class On_Collision : MonoBehaviour
         {
             Debug.DrawRay(contact.point, contact.normal, Color.white);
         }
-        print(collision.gameObject);
+        if (collision.gameObject.name == "Right Interactor" && setActive == false)
+        {
+            music.Play();
+            setActive = true;
+        }
+        else if (collision.gameObject.name == "Right Interactor" && setActive == true)
+        {
+            music.Stop();
+            setActive = false;
+        }
+        Debug.Log(collision.gameObject.name);
         //if (collision.relativeVelocity.magnitude > 2)
         //    audioSource.Play();
     }
