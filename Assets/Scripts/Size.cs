4e0ea882d12ce7f54c41b3d67bb206e6f91be90e
Edward <ereyes28@uic.edu>
2019-10-18 20:32:02 -0500

    Size Script

---
+++ Assets/Scripts/Size.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Size : MonoBehaviour
+{
+    //public GameObject lights;
+    private bool large;
+    private bool small;
+    private bool normal;
+    public GameObject roof;
+    public Transform trackedAlias;
+
+    //AudioSource sound;
+    //private bool setActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        large = false;
+        small = false;
+        normal = true;
+        roof.SetActive(true);
+
+        //lights.SetActive(true);
+        //sound = GetComponent<AudioSource>();
+        //setActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (normal == true && Input.GetKeyDown("b"))
+        {
+            Vector3 test = trackedAlias.localPosition;
+            
+            normal = false;
+            large = true;
+            trackedAlias.localScale += new Vector3(5, 5, 5);
+            trackedAlias.localPosition = new Vector3(-7, test.y, 20);
+            roof.SetActive(false);
+        }
+        else if (large == true && Input.GetKeyDown("l"))
+        {
+            Vector3 test = trackedAlias.localPosition;
+
+            normal = true;
+            large = false;
+            trackedAlias.localScale -= new Vector3(5, 5, 5);
+            trackedAlias.localPosition = new Vector3(0, test.y, 0);
+            roof.SetActive(true);
+
+            //trackedAlias.localPosition = new Vector3(-7, 0, 20);
+        }
+        else if (normal == true && Input.GetKeyDown("l"))
+        {
+            normal = false;
+            small = true;
+            trackedAlias.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        else if (small == true && Input.GetKeyDown("b"))
+        {
+            normal = true;
+            small = false;
+            trackedAlias.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Size_Button.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Size_Button : MonoBehaviour
+{
+    public GameObject big;
+    public GameObject small;
+    public GameObject normal;
+    public GameObject roof;
+    public Transform trackedAlias;
+
+    //AudioSource sound;
+    //private bool setActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*big = GameObject.Find("Sizes/Big");
+        small = GameObject.Find("Small");
+        normal= GameObject.Find("Normal");
+        roof = GameObject.Find("All Ceiling");
+        trackedAlias = GameObject.Find("TrackedAlias").transform;*/
+        //roof.SetActive(true);
+
+        //lights.SetActive(true);
+        //sound = GetComponent<AudioSource>();
+        //setActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Right Interactor" && normal.activeSelf == true && this.gameObject.name == "Grow Button")
+        {
+            Vector3 test = trackedAlias.localPosition;
+            normal.SetActive(false);
+            big.SetActive(true);
+            trackedAlias.localScale += new Vector3(5, 5, 5);
+            trackedAlias.localPosition = new Vector3(3, test.y, 10);
+            roof.SetActive(false);
+        }
+        else if (collision.gameObject.name == "Right Interactor" && normal.activeSelf == true && this.gameObject.name == "Shrink Button")
+        {
+            Vector3 test = trackedAlias.localPosition;
+            normal.SetActive(false);
+            small.SetActive(true);
+            trackedAlias.localScale -= new Vector3(0.8f, 0.8f, 0.8f);
+            trackedAlias.localPosition = new Vector3(0, test.y, -2);
+        }
+        else if (collision.gameObject.name == "Right Interactor" && big.activeSelf == true && this.gameObject.name == "Shrink Button")
+        {
+            Vector3 test = trackedAlias.localPosition;
+            big.SetActive(false);
+            normal.SetActive(true);
+            trackedAlias.localScale += new Vector3(5, 5, 5);
+            trackedAlias.localPosition = new Vector3(0, test.y, 0);
+            roof.SetActive(true);
+        }
+        else if (collision.gameObject.name == "Right Interactor" && small.activeSelf == true && this.gameObject.name == "Grow Button")
+        {
+            Vector3 test = trackedAlias.localPosition;
+            small.SetActive(false);
+            normal.SetActive(true);
+            trackedAlias.localScale += new Vector3(0.8f, 0.8f, 0.8f);
+            trackedAlias.localPosition = new Vector3(0, test.y, 0);
+        }
+        Debug.Log(collision.gameObject.name);
+        //if (collision.relativeVelocity.magnitude > 2)
+        //    audioSource.Play();
+    }
+}
